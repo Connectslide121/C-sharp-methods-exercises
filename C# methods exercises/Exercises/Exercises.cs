048af983a9f03d1e126aef2cc4edbf39d1936ec5
Jon Mendizabal <jonmendi@gmail.com>
2023-09-19 17:05:23 +0200

    Add project files.

---
+++ C# methods exercises/Exercises/Exercises.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__methods_exercises.Exercises
+{
+    internal class Exercises
+    {
+        public static string UserName() //**************EXERCISE 01***************
+        {
+            Console.WriteLine("What's your name?");
+            string userName = Console.ReadLine();
+           
+            return userName;
+        }
+
+
+
+        public static string Hashtag() //**************EXERCISE 02***************
+        {
+            Console.WriteLine("What hashtag would you like to create?");
+            string word = Console.ReadLine();
+            string hashtag = word.Insert(0, "#");
+            
+            return hashtag;
+        }
+
+        public static string Definition(string message) //**************EXERCISE 03***************
+        {
+            Console.WriteLine($"{message}");
+            string definition = Console.ReadLine();
+
+            return definition;
+        }
+
+        public static string Priority(string word) //**************EXERCISE 04***************
+        {
+            Console.WriteLine($"Please enter a {word}, this will determine the priority of your hashtag");
+            string priority = Console.ReadLine();
+
+            return priority;
+        }
+
+        public static string UserInfo(string firstName, string lastName) //**************EXERCISE 05***************
+        {
+            string userInfo = ($"{lastName}, {firstName}");
+            Console.WriteLine(userInfo);
+
+            return userInfo;
+        }
+    }
+}
---
+++ C# methods exercises/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using C__methods_exercises.Exercises;
+
+Exercises.UserName();
+
+Exercises.Hashtag();
+
+Exercises.Definition("What is the hashtag about?");
+
+Exercises.Priority("number");
+
+Exercises.UserInfo("Jon", "Mendizabal");
